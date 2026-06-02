23345c029650d6029bf03323877a784ceb25c9c1
Rishabh Gupta <guptarishabh8686@gmail.com>
2026-06-02 22:16:06 +0530

    Adding types in src i.e annotation and inference

---
+++ src/typesInTs.ts
@@ -0,0 +1,12 @@
+// Inference
+let drink = "chai"
+let cups = Math.random() > 0.5 ? 10: 5;
+let channelName = "Rishu"
+
+// type annotation
+
+let chaiFlaovour:string = "Masala chai"
+chaiFlaovour = "ginger chai"
+
+// let chaiOrder:2
+// let chai:boolean
